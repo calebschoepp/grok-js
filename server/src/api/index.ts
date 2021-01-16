6cc9bf9c676559254cdfb7e8415fec36c85f7ace
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-01-16 14:09:15 -0700

    Rough outline using acorn

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,9 +1,22 @@
-interface Region {
-    start: int;
-    end: int;
+import * as fs from 'fs';
+import * as acorn from 'acorn';
+
+export interface Region {
+    start: number;
+    end: number;
 }
 
-// TODO don't return any
+// TODO don't return any but rather a proper type
 export function grok(src: string, highlight: Region): any {
-    console.log(highlight);
+    // Parse the source code into an AST
+    const opts: acorn.Options = {
+        ecmaVersion: 2015,
+    };
+    const ast = acorn.parse(src, opts);
+
+    //
 }
+
+// TODO remove this is just for testing
+const highlight: Region = { start: 0, end: 10 };
+console.log(grok(fs.readFileSync('./server/src/api/example.js', 'utf8'), highlight));
