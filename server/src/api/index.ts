5c444fd10f7460847d6786d3d92796c57ec375f2
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-01-16 18:12:02 -0700

    Better algorithm approach

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,13 +2,15 @@ import * as fs from 'fs';
 import * as acorn from 'acorn';
 import * as walk from 'acorn-walk';
 
-export interface Region {
+export interface Selection {
     start: number;
     end: number;
 }
 
+// TODO build a memoize function
+
 // TODO don't return any but rather a proper type
-export function grok(src: string, highlight: Region): any {
+export function grok(src: string, selection: Selection, isHighlighting: boolean): string {
     // Parse the source code into an AST
     // TODO add more options?
     const opts: acorn.Options = {
@@ -23,36 +25,25 @@ export function grok(src: string, highlight: Region): any {
         return 'ERROR';
     }
 
-    // First try to grab the least specific node from an exact selection.
-    // If that fails try to grab the least specific node from an inexact selection.
     let found: walk.Found<acorn.Node> | undefined;
     try {
-        found = walk.findNodeAt(ast, highlight.start, highlight.end, anyNode);
-        if (!found) {
-            console.log('Less exact.');
-            found = walk.findNodeAfter(ast, highlight.start, anyNode);
-            if (!found) {
-                // Couldn't find anything, something went wrong
-                throw Error('Failed to find any node.');
+        if (isHighlighting) {
+            // Highlighting, so find the most specific node in selection
+            if ((found = walk.findNodeAt(ast, selection.start, selection.end, anyNode))) {
+                found = walk.findNodeAround(ast, selection.start, anyNode);
             }
+        } else {
+            // Not highlighting anything so find the least specific node after selection.start
+            found = walk.findNodeAfter(ast, selection.start, anyNode);
         }
     } catch (error) {
         // TODO return something else
         console.log('Failed to parse AST.');
         return 'ERROR';
     }
-    const subTree: acorn.Node = found.node;
+    const subTree: acorn.Node = (found as walk.Found<acorn.Node>).node;
 
-    console.log(subTree);
-
-    // console.log('\n\nwalk.findNodeAt:');
-    // console.log(walk.findNodeAt(ast, highlight.start, highlight.end, anyNode));
-    // console.log('\n\nwalk.findNodeAround:');
-    // console.log(walk.findNodeAround(ast, highlight.start, anyNode));
-    // console.log('\n\nwalk.findNodeAfter:');
-    // console.log(walk.findNodeAfter(ast, highlight.start, anyNode));
-
-    return '';
+    return subTree.type;
 }
 
 function anyNode(type: any, node: any): boolean {
@@ -60,14 +51,21 @@ function anyNode(type: any, node: any): boolean {
 }
 
 // TODO remove this is just for testing
-function test(highlight: Region, label: string) {
+function test(selection: Selection, label: string, isHighlighting: boolean) {
+    const src = fs.readFileSync('./server/src/api/example.js', 'utf8');
     console.log(label);
-    console.log(highlight);
-    console.log(grok(fs.readFileSync('./server/src/api/example.js', 'utf8'), highlight));
+    console.log('=============================================');
+    console.log(`"${src.substring(selection.start, selection.end)}"`);
+    console.log('=============================================');
+    console.log(selection);
+    console.log(grok(src, selection, false));
+    console.log('\n\n');
 }
 
-test({ start: 78, end: 85 }, 'Rest element');
-test({ start: 272, end: 302 }, 'Arrow function');
-test({ start: 530, end: 548 }, 'Function');
-test({ start: 381, end: 394 }, 'Identifier');
-test({ start: 509, end: 510 }, 'Binary and literal');
+// TODO better tests
+test({ start: 78, end: 85 }, 'Rest element', false);
+test({ start: 272, end: 302 }, 'Arrow function', false);
+test({ start: 530, end: 548 }, 'Function', false);
+test({ start: 381, end: 394 }, 'Identifier', false);
+test({ start: 509, end: 510 }, 'Binary and literal', false);
+test({ start: 383, end: 510 }, 'Mid spread', false);
